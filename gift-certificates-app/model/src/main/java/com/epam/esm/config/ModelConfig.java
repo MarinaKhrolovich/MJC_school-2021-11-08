babca217899947ad0e9a0c05286e3d3564260108
Marina <maniuneshka@gmail.com>
2021-12-06 20:57:51 +0300

    used db.properties for HikariConfig

--- gift-certificates-app/model/src/main/java/com/epam/esm/config/ModelConfig.java
+++ gift-certificates-app/model/src/main/java/com/epam/esm/config/ModelConfig.java
@@ -2,26 +2,42 @@ package com.epam.esm.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:properties/db.properties")
 @ComponentScan("com.epam.esm")
 public class ModelConfig {
+
+    @Value("${db.driverClassName}")
+    private String DB_DRIVER_CLASS_NAME;
+
+    @Value("${db.jdbcUrl}")
+    private String DB_JDBC_URL;
+
+    @Value("${db.username}")
+    private String DB_USERNAME;
+
+    @Value("${db.password}")
+    private String DB_PASSWORD;
 
     @Bean
     public DataSource dataSource() {
 
         HikariConfig config = new HikariConfig();
-        config.setDriverClassName("com.mysql.jdbc.Driver");
-        config.setUsername("root");
-        config.setPassword("12345");
-        config.setJdbcUrl("jdbc:mysql://127.0.0.1/gift-certificates?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        config.setDriverClassName(DB_DRIVER_CLASS_NAME);
+        config.setUsername(DB_USERNAME);
+        config.setPassword(DB_PASSWORD);
+        config.setJdbcUrl(DB_JDBC_URL);
+
         HikariDataSource dataSource = new HikariDataSource(config);
 
         return dataSource;

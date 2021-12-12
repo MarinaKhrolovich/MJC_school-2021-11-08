15660d57660755b00cc51558afae4b93011e9542
Marina <maniuneshka@gmail.com>
2021-12-13 02:12:43 +0300

    completed get of certificates with different parameters

--- gift-certificates-app/dao/src/main/java/com/epam/esm/dao/impl/CertificateDAOImpl.java
+++ gift-certificates-app/dao/src/main/java/com/epam/esm/dao/impl/CertificateDAOImpl.java
@@ -20,7 +20,6 @@ import java.util.List;
 @Repository
 public class CertificateDAOImpl implements CertificateDAO {
 
-    public static final String SELECT_FROM_CERTIFICATE = "SELECT * FROM certificate";
     public static final String SELECT_FROM_CERTIFICATE_WHERE_ID = "SELECT * FROM certificate WHERE id = ?";
     public static final String DELETE_FROM_CERTIFICATE_WHERE_ID = "DELETE FROM certificate WHERE id = ?";
     public static final String CREATE_CERTIFICATE = "INSERT INTO certificate(name,description,duration,price,create_date,last_update_date) VALUES(?,?,?,?,?,?)";
--- gift-certificates-app/dao/src/main/java/com/epam/esm/dao/util/CertificateGetSQLRequest.java
+++ gift-certificates-app/dao/src/main/java/com/epam/esm/dao/util/CertificateGetSQLRequest.java
@@ -8,7 +8,18 @@ import java.util.List;
 public class CertificateGetSQLRequest {
 
     public static final String SELECT_FROM_CERTIFICATE = "SELECT * FROM certificate";
-
+    public static final String JOIN = " JOIN certificate_tag JOIN tag ON certificate.id = certificate_tag.certificate_id AND certificate_tag.tag_id = tag.id";
+    public static final String WHERE = " WHERE";
+    public static final String CERTIFICATE_NAME = " certificate.name LIKE concat ('%', ?, '%')";
+    public static final String CERTIFICATE_DESCRIPTION = " certificate.description LIKE concat ('%', ?, '%')";
+    public static final String TAG_NAME = " tag.name = ?";
+    public static final String ORDER_BY = " ORDER BY";
+    public static final String ORDER_NAME = " certificate.name";
+    public static final String ORDER_DATE = " certificate.create_date";
+    public static final String ASC = " ASC";
+    public static final String DESC = " DESC";
+    public static final String AND = " AND";
+    public static final String COMMA = ", ";
 
     private String sqlRequest;
     private List<Object> parameters;
@@ -29,8 +40,55 @@ public class CertificateGetSQLRequest {
         stringBuilder.append(SELECT_FROM_CERTIFICATE);
         parameters = new ArrayList<>();
 
+        String tagName = requestParameters.getTagName();
+        String certificateName = requestParameters.getCertificateName();
+        String certificateDescription = requestParameters.getCertificateDescription();
+        String orderByDate = requestParameters.getOrderByDate();
+        String orderByName = requestParameters.getOrderByName();
+
+        boolean whereExists = false;
+        boolean orderExists = false;
+
+        if (tagName != null) {
+            stringBuilder.append(JOIN);
+        }
+
+        whereExists = appendWhere(stringBuilder, certificateName, CERTIFICATE_NAME, whereExists);
+        whereExists = appendWhere(stringBuilder, certificateDescription, CERTIFICATE_DESCRIPTION, whereExists);
+        appendWhere(stringBuilder, tagName, TAG_NAME, whereExists);
+
+        orderExists = appendOrder(stringBuilder, orderByDate, ORDER_DATE, orderExists);
+        appendOrder(stringBuilder, orderByName, ORDER_NAME, orderExists);
 
         sqlRequest = stringBuilder.toString();
+    }
+
+    private boolean appendOrder(StringBuilder stringBuilder, String orderBy, String sqlRequest, boolean orderExists) {
+        if (orderBy != null) {
+            if (!orderExists) {
+                stringBuilder.append(ORDER_BY);
+                orderExists = true;
+            } else {
+                stringBuilder.append(COMMA);
+            }
+            stringBuilder.append(sqlRequest);
+            stringBuilder.append(orderBy.equals(DESC.trim())?DESC:ASC);
+        }
+        return orderExists;
+    }
+
+    private boolean appendWhere(StringBuilder stringBuilder, String field, String sqlRequest, boolean whileExists) {
+        if (field != null) {
+            if (!whileExists) {
+                stringBuilder.append(WHERE);
+                whileExists = true;
+            } else {
+                stringBuilder.append(AND);
+            }
+            stringBuilder.append(sqlRequest);
+            parameters.add(field);
+        }
+        return whileExists;
     }
 }
 

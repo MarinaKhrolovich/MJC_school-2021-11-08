c195dfadc01d462a93423b0c5a2bdf846eaa9f29
Marina <maniuneshka@gmail.com>
2021-12-17 12:36:28 +0300

    added DataSourceTransactionManager to ConfigTest

--- gift-certificates-app/dao/src/test/java/com/epam/esm/config/ConfigTest.java
+++ gift-certificates-app/dao/src/test/java/com/epam/esm/config/ConfigTest.java
@@ -2,14 +2,17 @@ package com.epam.esm.config;
 
 import org.springframework.context.annotation.*;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Profile("dev")
 @Configuration
 @ComponentScan("com.epam.esm")
+@EnableTransactionManagement
 public class ConfigTest {
 
     public static final String UTF_8 = "UTF-8";
@@ -29,5 +32,11 @@ public class ConfigTest {
         return new JdbcTemplate(dataSource());
     }
 
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource);
+        return dataSourceTransactionManager;
+    }
 
 }

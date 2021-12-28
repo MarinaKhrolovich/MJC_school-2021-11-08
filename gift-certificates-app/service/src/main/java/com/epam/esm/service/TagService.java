bb2634adc9011d75f02cabe5802af5ff1f90a140
Marina <maniuneshka@gmail.com>
2021-12-29 01:02:42 +0300

    added descripton for javadoc (TagService,CertificateService)

--- gift-certificates-app/service/src/main/java/com/epam/esm/service/TagService.java
+++ gift-certificates-app/service/src/main/java/com/epam/esm/service/TagService.java
@@ -4,13 +4,34 @@ import com.epam.esm.bean.Tag;
 
 import java.util.List;
 
+/**
+ * Service interface responsible for CRD operation with object Tag
+ * @author Marina Khrolovich
+ */
 public interface TagService {
 
+    /**
+     * Add Tag to the database according to provided object Tag
+     * @param tag is object {@link Tag}
+     */
     void add(Tag tag);
 
+    /**
+     * Get Tag from the database according to provided id
+     * @param id is id of Tag {@link Tag} to be getting
+     * @return tag is object {@link Tag}
+     */
     Tag get(int id);
 
+    /**
+     * Get all Tags from the database
+     * @return list of tags {@link Tag}
+     */
     List<Tag> get();
 
+    /**
+     * Delete Tag from the database according to provided id
+     * @param id is id of Tag to be deleting
+     */
     void delete(int id);
 }

7ba58bbba8acce3202490f6bde01ee92c15eff7e
Marina <maniuneshka@gmail.com>
2022-01-08 00:08:31 +0300

    add CertificateUpdateDTO, change CertificateMapper

---
+++ gift-certificates-app/service/src/main/java/com/epam/esm/dto/CertificateUpdateDTO.java
@@ -0,0 +1,41 @@
+package com.epam.esm.dto;
+
+import lombok.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.time.Instant;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class CertificateUpdateDTO {
+
+    private int id;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "{message.certificate.name.fill}")
+    @Size(min = 3, max = 100, message = "{message.certificate.name.length}")
+    private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "{message.certificate.description.fill}")
+    @Size(min = 3, max = 1000, message = "{message.certificate.description.length}")
+    private String description;
+
+    @DecimalMin(value = "0.01", message = "{message.certificate.price}")
+    private double price;
+
+    @Min(value = 1, message = "{message.certificate.duration}")
+    private int duration;
+
+    private Instant createDate;
+
+    private Instant lastUpdateDate;
+
+    @Valid
+    private List<TagDTO> tagList;
+
+}
--- gift-certificates-app/service/src/main/java/com/epam/esm/mapper/CertificateMapper.java
+++ gift-certificates-app/service/src/main/java/com/epam/esm/mapper/CertificateMapper.java
@@ -3,6 +3,7 @@ package com.epam.esm.mapper;
 import com.epam.esm.bean.Certificate;
 import com.epam.esm.bean.Tag;
 import com.epam.esm.dto.CertificateDTO;
+import com.epam.esm.dto.CertificateUpdateDTO;
 import com.epam.esm.dto.TagDTO;
 import org.mapstruct.Mapper;
 
@@ -11,5 +12,8 @@ public interface CertificateMapper {
 
     Certificate сonvertToEntity(CertificateDTO certificateDTO);
 
+    Certificate сonvertToEntity(CertificateUpdateDTO certificateUpdateDTO);
+
     Tag сonvertToEntity(TagDTO tagDTO);
+
 }

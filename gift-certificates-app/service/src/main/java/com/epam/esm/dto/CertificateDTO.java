aae7e374af29c317d9665a9ee85d433b4cb7b406
Marina <maniuneshka@gmail.com>
2022-01-07 23:10:07 +0300

    add hibernate validation to CertificateDTO

--- gift-certificates-app/service/src/main/java/com/epam/esm/dto/CertificateDTO.java
+++ gift-certificates-app/service/src/main/java/com/epam/esm/dto/CertificateDTO.java
@@ -2,6 +2,11 @@ package com.epam.esm.dto;
 
 import lombok.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.Instant;
 import java.util.List;
 
@@ -15,18 +20,25 @@ public class CertificateDTO {
 
     private int id;
 
+    @NotBlank(message = "{message.certificate.name.fill}")
+    @Size(min = 3, max = 100, message = "{message.certificate.name.length}")
     private String name;
 
+    @NotBlank(message = "{message.certificate.description.fill}")
+    @Size(min = 3, max = 1000, message = "{message.certificate.description.length}")
     private String description;
 
+    @DecimalMin(value="0.01", message="{message.certificate.price}")
     private double price;
 
+    @Min(value=1, message="{message.certificate.duration}")
     private int duration;
 
     private Instant createDate;
 
     private Instant lastUpdateDate;
 
+    @Valid
     private List<TagDTO> tagList;
 
 }

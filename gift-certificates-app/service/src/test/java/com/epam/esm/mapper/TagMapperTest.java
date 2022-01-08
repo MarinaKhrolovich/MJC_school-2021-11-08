87266a00641eafe21bd1731996224ef436033956
Marina <maniuneshka@gmail.com>
2022-01-09 00:08:54 +0300

    add TagMapperTest

---
+++ gift-certificates-app/service/src/test/java/com/epam/esm/mapper/TagMapperTest.java
@@ -0,0 +1,51 @@
+package com.epam.esm.mapper;
+
+import com.epam.esm.bean.Tag;
+import com.epam.esm.config.ServiceConfig;
+import com.epam.esm.dto.TagDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {ServiceConfig.class})
+class TagMapperTest {
+
+    public static final String NEW_TAG = "new tag";
+    public static final int ID_TAG = 1;
+
+    private final TagMapper tagMapper;
+
+    @Autowired
+    TagMapperTest(TagMapper tagMapper) {
+        this.tagMapper = tagMapper;
+    }
+
+    @Test
+    void сonvertToEntity() {
+        TagDTO tagDTO = new TagDTO();
+        tagDTO.setId(ID_TAG);
+        tagDTO.setName(NEW_TAG);
+
+        Tag actualTag = tagMapper.сonvertToEntity(tagDTO);
+
+        assertEquals(tagDTO.getId(), actualTag.getId());
+        assertEquals(tagDTO.getName(), actualTag.getName());
+    }
+
+    @Test
+    void сonvertToDTO() {
+        Tag tag = new Tag();
+        tag.setId(ID_TAG);
+        tag.setName(NEW_TAG);
+
+        TagDTO actualTagDTO = tagMapper.сonvertToDTO(tag);
+
+        assertEquals(tag.getId(), actualTagDTO.getId());
+        assertEquals(tag.getName(), actualTagDTO.getName());
+    }
+}

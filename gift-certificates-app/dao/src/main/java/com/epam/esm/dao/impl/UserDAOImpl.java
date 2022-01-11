34bfcef50765cb3993d108e33adac7993485d850
Marina <maniuneshka@gmail.com>
2022-01-11 21:22:38 +0300

    implement UserDAOImpl

--- gift-certificates-app/dao/src/main/java/com/epam/esm/dao/impl/UserDAOImpl.java
+++ gift-certificates-app/dao/src/main/java/com/epam/esm/dao/impl/UserDAOImpl.java
@@ -2,26 +2,65 @@ package com.epam.esm.dao.impl;
 
 import com.epam.esm.bean.User;
 import com.epam.esm.dao.UserDAO;
+import com.epam.esm.exception.ResourceAlreadyExistsException;
+import com.epam.esm.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
 
+    private static final String SELECT_FROM_USER = "SELECT * FROM user";
+    private static final String SELECT_FROM_USER_WHERE_ID = "SELECT * FROM user WHERE id = ?";
+    private static final String CREATE_USER = "INSERT INTO user(login, name, surname) VALUES(?,?,?)";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserDAOImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public User add(User user) {
-        return null;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        try {
+            jdbcTemplate.update(
+                    connection -> {
+                        PreparedStatement ps =
+                                connection.prepareStatement(CREATE_USER, new String[]{"id"});
+                        ps.setString(1, user.getLogin());
+                        ps.setString(2, user.getName());
+                        ps.setString(3, user.getSurname());
+                        return ps;
+                    },
+                    keyHolder);
+        } catch (DuplicateKeyException exception) {
+            throw new ResourceAlreadyExistsException(user.getLogin());
+        }
+        if (keyHolder.getKey() != null) {
+            user.setId(keyHolder.getKey().intValue());
+        }
+        return user;
     }
 
     @Override
     public User get(int id) {
-        return null;
+        return jdbcTemplate.query(SELECT_FROM_USER_WHERE_ID, new BeanPropertyRowMapper<>(User.class), id).stream()
+                .findAny().orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     @Override
     public List<User> get() {
-        return null;
+        return jdbcTemplate.query(SELECT_FROM_USER, new BeanPropertyRowMapper<>(User.class));
     }
 
 }

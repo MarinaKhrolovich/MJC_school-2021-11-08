699b799611085deaf7d092f9b436d00799d90e33
Marina <maniuneshka@gmail.com>
2022-01-13 16:31:08 +0300

    correct SpringConfig: use @SpringBootApplication+correct all pom.xml

--- gift-certificates-app/web/src/main/java/com/epam/esm/config/SpringConfig.java
+++ gift-certificates-app/web/src/main/java/com/epam/esm/config/SpringConfig.java
@@ -3,24 +3,26 @@ package com.epam.esm.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@ComponentScan("com.epam.esm")
-@EnableWebMvc
+@SpringBootApplication
 public class SpringConfig implements WebMvcConfigurer {
 
     public static final String UTF_8 = "UTF-8";
     public static final String LOCALIZATION_LOCAL = "localization/local";
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfig.class, args);
+    }
 
     @Bean
     public MessageSource messageSource() {

2638d6a05f40f11350c9421ab131c26f8476fac8
Marina <maniuneshka@gmail.com>
2022-01-13 22:04:13 +0300

    change SpringConfig,ConfigDAO + properties (spring.datasource.)

--- gift-certificates-app/dao/src/main/java/com/epam/esm/config/ConfigDAO.java
+++ gift-certificates-app/dao/src/main/java/com/epam/esm/config/ConfigDAO.java
@@ -1,48 +1,23 @@
 package com.epam.esm.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Profile("!test")
-@Configuration
 @PropertySource({"classpath:properties/application.properties", "classpath:properties/application-${spring.profiles.active}.properties"})
-@ComponentScan("com.epam.esm")
 @EnableTransactionManagement
+@SpringBootApplication
 public class ConfigDAO {
 
-    private final Environment env;
-
-    @Value("${db.poolsize:5}")
-    private int MAX_POOL_SIZE;
-
-    @Autowired
-    public ConfigDAO(Environment env) {
-        this.env = env;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setDriverClassName(env.getProperty("db.driverClassName"));
-        config.setUsername(env.getProperty("db.username"));
-        config.setPassword(env.getProperty("db.password"));
-        config.setJdbcUrl(env.getProperty("db.jdbcUrl"));
-        config.setMaximumPoolSize(MAX_POOL_SIZE);
-        return new HikariDataSource(config);
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigDAO.class, args);
     }
 
     @Bean
--- gift-certificates-app/web/src/main/java/com/epam/esm/config/SpringConfig.java
+++ gift-certificates-app/web/src/main/java/com/epam/esm/config/SpringConfig.java
@@ -1,16 +1,10 @@
 package com.epam.esm.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.validation.Validator;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -18,7 +12,6 @@ public class SpringConfig implements WebMvcConfigurer {
 
     public static final String UTF_8 = "UTF-8";
     public static final String LOCALIZATION_LOCAL = "localization/local";
-
 
     public static void main(String[] args) {
         SpringApplication.run(SpringConfig.class, args);
@@ -30,26 +23,6 @@ public class SpringConfig implements WebMvcConfigurer {
         messageSource.setBasename(LOCALIZATION_LOCAL);
         messageSource.setDefaultEncoding(UTF_8);
         return messageSource;
-    }
-
-    @Override
-    public Validator getValidator() {
-        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
-        validator.setValidationMessageSource((MessageSource) messageSource());
-        return validator;
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        return objectMapper;
-    }
-
-    @Bean
-    public MethodValidationPostProcessor methodValidationPostProcessor() {
-        return new MethodValidationPostProcessor();
     }
 
 }

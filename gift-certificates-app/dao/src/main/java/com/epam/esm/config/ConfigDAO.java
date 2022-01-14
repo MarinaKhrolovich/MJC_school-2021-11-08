c94a944d62510796abe52855daeffdc49143ef27
Marina <maniuneshka@gmail.com>
2022-01-14 11:08:18 +0300

    delete DataSourceTransactionManager from ConfigDAO

--- gift-certificates-app/dao/src/main/java/com/epam/esm/config/ConfigDAO.java
+++ gift-certificates-app/dao/src/main/java/com/epam/esm/config/ConfigDAO.java
@@ -2,29 +2,16 @@ package com.epam.esm.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @Profile("!test")
 @PropertySource({"classpath:properties/application.properties", "classpath:properties/application-${spring.profiles.active}.properties"})
-@EnableTransactionManagement
 @SpringBootApplication
 public class ConfigDAO {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigDAO.class, args);
-    }
-
-    @Bean
-    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
-        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
-        dataSourceTransactionManager.setDataSource(dataSource);
-        return dataSourceTransactionManager;
     }
 
 }

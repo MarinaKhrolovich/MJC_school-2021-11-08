3a7f0cfcc21dd7fc639f6d6590abbeb4dda8107d
Marina <maniuneshka@gmail.com>
2022-01-20 19:05:17 +0300

    correct JsonInclude.Include.NON_NULL at UserDTO,CertificateDTO

--- gift-certificates-app/service/src/main/java/com/epam/esm/dto/CertificateDTO.java
+++ gift-certificates-app/service/src/main/java/com/epam/esm/dto/CertificateDTO.java
@@ -12,40 +12,34 @@ import java.time.Instant;
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CertificateDTO extends RepresentationModel<CertificateDTO> {
 
     private int id;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     @NotBlank(message = "{message.certificate.name.fill}")
     @Size(min = 3, max = 100, message = "{message.certificate.name.length}")
     private String name;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     @NotBlank(message = "{message.certificate.description.fill}")
     @Size(min = 3, max = 1000, message = "{message.certificate.description.length}")
     private String description;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     @NotNull(message = "{message.certificate.price.fill")
     @DecimalMin(value = "0.01", message = "{message.certificate.price.value}")
     private BigDecimal price;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     @NotNull(message = "{message.certificate.duration.fill")
     @Min(value = 1, message = "{message.certificate.duration.value}")
     private Integer duration;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS", timezone = "UTC")
     private Instant createDate;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS", timezone = "UTC")
     private Instant lastUpdateDate;
 
     @Valid
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<TagDTO> tagList;
 
 }
--- gift-certificates-app/service/src/main/java/com/epam/esm/dto/UserDTO.java
+++ gift-certificates-app/service/src/main/java/com/epam/esm/dto/UserDTO.java
@@ -8,20 +8,18 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDTO extends RepresentationModel<UserDTO> {
 
     private int id;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     @NotBlank(message = "{message.user.login.fill}")
     @Size(min = 3, max = 45, message = "{message.user.login.length}")
     private String login;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     @Size(min = 3, max = 45, message = "{message.user.name.length}")
     private String name;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     @Size(min = 3, max = 45, message = "{message.user.surname.length}")
     private String surname;
 

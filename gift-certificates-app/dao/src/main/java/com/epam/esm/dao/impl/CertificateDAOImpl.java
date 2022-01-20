76e2049e14cbe32d38f9a3ef7c4551ca78ac455e
Marina <maniuneshka@gmail.com>
2022-01-21 02:45:37 +0300

    change update of Certificate: use addTagIfNotExists + add BeanUtils.copyProperties

--- gift-certificates-app/dao/src/main/java/com/epam/esm/dao/impl/CertificateDAOImpl.java
+++ gift-certificates-app/dao/src/main/java/com/epam/esm/dao/impl/CertificateDAOImpl.java
@@ -9,6 +9,9 @@ import com.epam.esm.dao.TagDAO;
 import com.epam.esm.exception.ResourceAlreadyExistsException;
 import com.epam.esm.exception.ResourceHasLinks;
 import com.epam.esm.exception.ResourceNotFoundException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,8 +20,10 @@ import org.springframework.util.CollectionUtils;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 @Transactional
@@ -58,10 +63,12 @@ public class CertificateDAOImpl implements CertificateDAO {
 
     @Override
     public Certificate update(int id, Certificate certificate) {
-        if (notExists(id)) {
-            throw new ResourceNotFoundException(id);
-        }
         certificate.setId(id);
+        addTagIfNotExists(certificate.getTagList());
+
+        Certificate certificateFromBase = get(id);
+        BeanUtils.copyProperties(certificateFromBase, certificate, getNotNullPropertyNames(certificate));
+
         entityManager.merge(certificate);
         return certificate;
     }
@@ -78,10 +85,6 @@ public class CertificateDAOImpl implements CertificateDAO {
         }
     }
 
-    private boolean notExists(int id) {
-        return Optional.ofNullable(entityManager.find(Certificate.class, id)).isEmpty();
-    }
-
     private void addTagIfNotExists(List<Tag> tags) {
         if (!CollectionUtils.isEmpty(tags)) {
             for (Tag tag : tags) {
@@ -93,6 +96,19 @@ public class CertificateDAOImpl implements CertificateDAO {
                 }
             }
         }
+    }
+
+    private static String[] getNotNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue != null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
     }
 
 }

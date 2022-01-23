cf2216988e0f32a137ccdf49745ade483e51d064
Marina <maniuneshka@gmail.com>
2022-01-23 15:35:56 +0300

    add class Application with main, change SpringConfig+pom.xml(main class)

--- gift-certificates-app/web/src/main/java/com/epam/esm/config/SpringConfig.java
+++ gift-certificates-app/web/src/main/java/com/epam/esm/config/SpringConfig.java
@@ -1,21 +1,15 @@
 package com.epam.esm.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
-public class SpringConfig implements WebMvcConfigurer {
+@Configuration
+public class SpringConfig {
 
     public static final String UTF_8 = "UTF-8";
     public static final String LOCALIZATION_LOCAL = "localization/local";
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringConfig.class, args);
-    }
 
     @Bean
     public MessageSource messageSource() {

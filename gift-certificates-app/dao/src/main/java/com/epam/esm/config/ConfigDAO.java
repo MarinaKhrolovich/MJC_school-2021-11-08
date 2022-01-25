a0595c631e525af911f64d6a4d72eaa2410b7588
Marina <maniuneshka@gmail.com>
2022-01-25 15:03:16 +0300

    delete from ConfigDAO bean DataSource+delete

--- gift-certificates-app/dao/src/main/java/com/epam/esm/config/ConfigDAO.java
+++ gift-certificates-app/dao/src/main/java/com/epam/esm/config/ConfigDAO.java
@@ -3,39 +3,16 @@ package com.epam.esm.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @PropertySource({"classpath:properties/application.properties",
         "classpath:properties/application-${spring.profiles.active}.properties"})
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EntityScan("com.epam.esm.bean")
 public class ConfigDAO {
 
-    private final Environment env;
-
-    public ConfigDAO(Environment env) {
-        this.env = env;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigDAO.class, args);
-    }
-
-    @Bean
-    public DataSource getDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-        dataSource.setUrl(env.getProperty("spring.datasource.url"));
-        dataSource.setUsername(env.getProperty("spring.datasource.username"));
-        dataSource.setPassword(env.getProperty("spring.datasource.password"));
-        return dataSource;
     }
 
 }

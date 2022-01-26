9d9329da99824173054a51deb60ca64e979ecd45
Marina <maniuneshka@gmail.com>
2022-01-26 12:23:29 +0300

    delete main from ConfigDAO, correct annotations

--- gift-certificates-app/dao/src/main/java/com/epam/esm/config/ConfigDAO.java
+++ gift-certificates-app/dao/src/main/java/com/epam/esm/config/ConfigDAO.java
@@ -1,18 +1,15 @@
 package com.epam.esm.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource({"classpath:properties/application.properties",
         "classpath:properties/application-${spring.profiles.active}.properties"})
-@SpringBootApplication
+@Configuration
+@ComponentScan("com.epam.esm")
 @EntityScan("com.epam.esm.bean")
 public class ConfigDAO {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigDAO.class, args);
-    }
 
 }

1de94330f577dcc2070d35ea857c3ff9ebf0717f
Marina <maniuneshka@gmail.com>
2022-02-03 21:56:24 +0300

    change SecurityConfig: permitAll for HttpMethod.GET(certificates,tags)

--- gift-certificates-app/web/src/main/java/com/epam/esm/config/SecurityConfig.java
+++ gift-certificates-app/web/src/main/java/com/epam/esm/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.epam.esm.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,16 +29,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http
                 .authorizeRequests()
-                .antMatchers("/users").permitAll()
+                .antMatchers(HttpMethod.GET, "/certificates/**","/tags/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .permitAll()
-                .and()
-                .rememberMe()
                 .and()
                 .logout()
                 .permitAll();

e1e487e3d647313da291182a3a410707b54ab3ec
Marina <maniuneshka@gmail.com>
2022-02-04 14:53:41 +0300

    create JwtTokenFilter

---
+++ gift-certificates-app/web/src/main/java/com/epam/esm/security/JwtTokenFilter.java
@@ -0,0 +1,20 @@
+package com.epam.esm.security;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class JwtTokenFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+}

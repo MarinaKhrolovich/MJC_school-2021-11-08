963a12cfe008466b7a2f7af3d54f3195feaab02e
Marina <maniuneshka@gmail.com>
2022-02-08 16:37:23 +0300

    add createJwtToken to JwtTokenService; add parameters for token to application.properties

--- gift-certificates-app/web/src/main/java/com/epam/esm/security/JwtTokenService.java
+++ gift-certificates-app/web/src/main/java/com/epam/esm/security/JwtTokenService.java
@@ -1,8 +1,6 @@
 package com.epam.esm.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,21 +10,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Optional;
 
 @Component
 public class JwtTokenService {
 
-    private static final String JWT_TOKEN_HEADER_NAME = "X-Auth-Token";
-
     @Value("${jwt.token.secret-key:secret}")
     private String secretKey;
 
     @Value("${jwt.token.expiration}")
-    private String expiration;
+    private long expirationSec;
+
+    @Value("${jwt.token.header-name}")
+    private String tokenHeaderName;
 
     private final UserDetailsService userService;
 
@@ -35,9 +36,26 @@ public class JwtTokenService {
         this.userService = userService;
     }
 
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createJwtToken(UserDetails userDetails) {
+        Claims claims = Jwts.claims().setSubject(userDetails.getUsername());
+        claims.put("authorities", userDetails.getAuthorities());
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime()+expirationSec))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
     public Authentication getAuthentication(HttpServletRequest servletRequest) {
         Optional<String> jwtToken = Optional
-                .ofNullable(servletRequest.getHeader(servletRequest.getHeader(JWT_TOKEN_HEADER_NAME)));
+                .ofNullable(servletRequest.getHeader(servletRequest.getHeader(tokenHeaderName)));
         if (jwtToken.isPresent()) {
             String jwtTokenValue = jwtToken.get();
             if (validateJwtToken(jwtTokenValue)) {
@@ -54,8 +72,12 @@ public class JwtTokenService {
     }
 
     public boolean validateJwtToken(String token) {
-        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
-        return claimsJws.getBody().getExpiration().after(Date.from(Instant.now()));
+        try {
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return claimsJws.getBody().getExpiration().after(Date.from(Instant.now()));
+        } catch (JwtException | IllegalArgumentException e){
+            throw new JwtAuthenticationException("");
+        }
     }
 
 }

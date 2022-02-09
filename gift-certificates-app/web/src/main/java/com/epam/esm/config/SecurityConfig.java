e36350f86c5fbdaeac62fa071f9c62b8b950dc5a
Marina <maniuneshka@gmail.com>
2022-02-09 15:25:08 +0300

    delete bean bCryptPasswordEncoder() from SecurityConfig

--- gift-certificates-app/web/src/main/java/com/epam/esm/config/SecurityConfig.java
+++ gift-certificates-app/web/src/main/java/com/epam/esm/config/SecurityConfig.java
@@ -1,30 +1,19 @@
 package com.epam.esm.config;
 
-import com.epam.esm.bean.Role;
 import com.epam.esm.security.JwtTokenFilter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
